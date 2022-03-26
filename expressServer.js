d8c05842621e32a0986bf913ea184eb3a754bb7e
dredshep <dredshep@gmail.com>
2022-03-26 00:51:53 -0400

    mm

---
+++ expressServer.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const { drawWatermarkAsBuffer } = require("./usingcanvas");
+
+// url 1 = https://image.tmdb.org/t/p/original//m03jul0YdVEOFXEQVUv6pOVQYGL.jpg
+// url 2 = ./AstroPeliculas.png
+
+/**const drawWatermarkAsBuffer = async (
+  pathOfSourceImageFile,
+  pathOfWaterMarkImageFile,
+  width,
+  height
+) => Buffer */
+
+app.get("/", async (req, res) => {
+  // grab url as https://website.com/?url=https://image.tmdb.org/t/p/original//m03jul0YdVEOFXEQVUv6pOVQYGL.jpg
+  const url = req.query.url;
+  if (url) {
+    const buffer = await drawWatermarkAsBuffer(
+      // "https://image.tmdb.org/t/p/original//m03jul0YdVEOFXEQVUv6pOVQYGL.jpg",
+      url,
+      "./AstroPeliculas.png",
+      1280,
+      720
+    );
+    res.send(`<img src="data:image/png;base64,${buffer.toString("base64")}">`);
+  } else {
+    res.send(
+      '<h1 style="font-size: 120px;font-family:sans-serif;">No url provided hehehe</h1>'
+    );
+  }
+});
+
+app.listen(6515, () => console.log("App launched on http://localhost:6515"));
---
+++ usingcanvas.js
@@ -0,0 +1,27 @@
+const { createCanvas, loadImage } = require("canvas");
+
+const drawWatermarkAsBuffer = async (
+  pathOfSourceImageFile,
+  pathOfWaterMarkImageFile,
+  width,
+  height
+) => {
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext("2d");
+  ctx.globalAlpha = 1;
+
+  // loading the source image file
+  const loadSourceImage = await loadImage(pathOfSourceImageFile);
+  // drawing the source file into canvas
+  ctx.drawImage(loadSourceImage, 0, 0, width, height);
+  ctx.globalAlpha = 0.5;
+  const loadWatermarkImage = await loadImage(pathOfWaterMarkImageFile);
+  // drawing the watermark image over the source image
+  //                                x 		y     w 	 h
+  ctx.drawImage(loadWatermarkImage, 987, 427, 300, 300);
+  return canvas.toBuffer();
+};
+
+module.exports = {
+  drawWatermarkAsBuffer,
+};
